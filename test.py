964d2c24ded1ab4d4c18981912a2ed6a1c98e14c
RSeto14 <107923048+RSeto14@users.noreply.github.com>
2023-06-08 11:03:14 +0900

    Update test.py

--- test.py
+++ test.py
@@ -11,35 +11,39 @@ controller = Controller(dt=0.005)
 start = 1000
 end = 1000
 
-for step in range(start + 10000 + end):
-    
-    if step < start:
-        env.render()
-        target = [0.65,0.8]
-        theta = obs
-        action = controller.control(theta=np.array(theta), x_target=np.array(target), kp=3, kd=0.1)
-        obs, reward, done, info = env.step(action)
+def test():
+    for step in range(start + 10000 + end):
 
-    elif start <= step < start +10000:
-        env.render()
-        t = step - start
+        if step < start:
+            env.render()
+            target = [0.65,0.8]
+            theta = obs
+            action = controller.control(theta=np.array(theta), x_target=np.array(target), kp=3, kd=0.1)
+            obs, reward, done, info = env.step(action)
 
-        #tを媒介変数にしたx,z###############################################
+        elif start <= step < start +10000:
+            env.render()
+            t = step - start
 
-        target = [0.1*sin(2*pi*t/1000) + 0.65, 0.1*cos(2*pi*t/1000) + 0.7]
-        
-        ##################################################################
-        theta = obs
-        action = controller.control(theta=np.array(theta), x_target=np.array(target), kp=3, kd=0.1)
-        obs, reward, done, info = env.step(action)
+            #tを媒介変数にしたx,z###############################################
+
+            target = [0.1*sin(2*pi*t/1000) + 0.65, 0.1*cos(2*pi*t/1000) + 0.7]
+
+            ##################################################################
+            theta = obs
+            action = controller.control(theta=np.array(theta), x_target=np.array(target), kp=3, kd=0.1)
+            obs, reward, done, info = env.step(action)
+        else:
+            env.render()
+
+            theta = obs
+            action = controller.control(theta=np.array(theta), x_target=np.array(target), kp=3, kd=0.1)
+            obs, reward, done, info = env.step(action)
+
+
+
     else:
-        env.render()
-
-        theta = obs
-        action = controller.control(theta=np.array(theta), x_target=np.array(target), kp=3, kd=0.1)
-        obs, reward, done, info = env.step(action)
-
-
-
-else:
-    print("finished")
+        print("finished")
+        
+if __name__=="__main__":
+    test()
